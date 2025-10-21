99fcdf1c75d34e189cca89b8637bb90906ca2116
Sam-MARTis <samanthmartis@gmail.com>
2025-10-21 15:27:10 +0530

    Basic imgui sfml working

--- Testing_Grounds/Testing_IMGUI/main.cpp
+++ Testing_Grounds/Testing_IMGUI/main.cpp
@@ -4,13 +4,23 @@
 
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
-
+#define RADIUS 100.f
+// using namespace ImGui::SFML;
+#define CLEAR_COLOUR sf::Color(50, 50, 50)
 int main() {
-    // In SFML 3, VideoMode is a struct; construct using brace initialization
     sf::RenderWindow window(sf::VideoMode({SCREEN_WIDTH, SCREEN_HEIGHT}, 10), "Fluid Simulation");
+    window.setFramerateLimit(60);
+    ImGui::SFML::Init(window);
+    sf::CircleShape shape(RADIUS);
+    shape.setFillColor(sf::Color::Green);
+    shape.setPosition({(SCREEN_WIDTH/2 - RADIUS) , (SCREEN_HEIGHT/2 - RADIUS)});
+
+
+    sf::Clock deltaClock;
 
     while (window.isOpen()) {
         while(const std::optional<sf::Event> event = window.pollEvent()) {
+            ImGui::SFML::ProcessEvent(window, *event);
             if(event->is<sf::Event::Closed>()) {
                 window.close();
             }
@@ -20,5 +30,21 @@ int main() {
                 }
             }
         }
+
+        ImGui::SFML::Update(window, deltaClock.restart());
+
+            ImGui::Begin("Hello, world!");
+            ImGui::Text("This is some useful text.");
+            ImGui::End();
+
+
+        window.clear(CLEAR_COLOUR);
+        window.draw(shape);
+        ImGui::SFML::Render(window);
+        window.display();
     }
+
+
+    ImGui::SFML::Shutdown();
+    return 0;
 }
