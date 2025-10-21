8dc60884ba60f91a34fccb8b144cc855e82a9fa0
Sam-MARTis <samanthmartis@gmail.com>
2025-10-21 15:13:19 +0530

    Got sfml 3.0 to work

--- Testing_Grounds/Testing_IMGUI/main.cpp
+++ Testing_Grounds/Testing_IMGUI/main.cpp
@@ -1,60 +1,24 @@
 #include <SFML/Graphics.hpp>
-#include <imgui.h>
-#include <imgui-SFML.h>
+#include "imgui.h"
+#include "imgui-SFML.h"
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML + ImGui Test");
-    window.setFramerateLimit(60);
-
-    if (!ImGui::SFML::Init(window)) {
-        return -1; // fail if ImGui-SFML fails to initialize
-    }
-
-    sf::Clock deltaClock;
-    sf::CircleShape circle(50.f);
-    circle.setFillColor(sf::Color::Cyan);
-    circle.setOrigin(50.f, 50.f);
-    circle.setPosition(400.f, 300.f);
-
-    float radius = 50.f;
-    float speed = 100.f;
-    bool animate = true;
-    sf::Color color = sf::Color::Cyan;
+    // In SFML 3, VideoMode is a struct; construct using brace initialization
+    sf::RenderWindow window(sf::VideoMode({SCREEN_WIDTH, SCREEN_HEIGHT}, 10), "Fluid Simulation");
 
     while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            ImGui::SFML::ProcessEvent(window, event); // <-- pass window as first argument
-            if (event.type == sf::Event::Closed)
+        while(const std::optional<sf::Event> event = window.pollEvent()) {
+            if(event->is<sf::Event::Closed>()) {
                 window.close();
+            }
+            else if(const auto* keyPressed = event->getIf<sf::Event::KeyPressed>()) {
+                if(keyPressed->scancode == sf::Keyboard::Scancode::Escape) {
+                    window.close();
+                }
+            }
         }
-
-        // ImGui::SFML::Update(window, deltaClock.restart());
-
-        // ImGui::Begin("Control Panel");
-        // ImGui::Text("Adjust the circle parameters:");
-        // ImGui::SliderFloat("Radius", &radius, 10.f, 200.f);
-        // ImGui::SliderFloat("Speed", &speed, 10.f, 500.f);
-        // ImGui::Checkbox("Animate", &animate);
-        // ImGui::ColorEdit3("Colour", reinterpret_cast<float*>(&color));
-        // ImGui::End();
-
-        circle.setRadius(radius);
-        circle.setOrigin(radius, radius);
-        circle.setFillColor(color);
-
-        static float x = 400.f;
-        float dt = deltaClock.getElapsedTime().asSeconds();
-        if (animate) x += speed * dt;
-        if (x > 800 + radius) x = -radius;
-        circle.setPosition(x, 300.f);
-
-        window.clear(sf::Color::Black);
-        window.draw(circle);
-        ImGui::SFML::Render(window);
-        window.display();
     }
-
-    ImGui::SFML::Shutdown();
-    return 0;
 }
